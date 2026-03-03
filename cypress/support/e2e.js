3a8ae85287db380450d1a4c5888caa0ed26024fa
Albert-Bolanos <alberto.bolanos@quind.io>
2026-03-03 18:22:44 -0500

    feat: Migre la ejecución de Cypress CI para usar la acción oficial de GitHub

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,14 +2,11 @@
 import './commands';
 import "allure-cypress";
 
-// This file is processed and loaded automatically before your test files.
 Cypress.on('uncaught:exception', (err, runnable) => {
-    // Retornar false previene que Cypress falle el test por errores no capturados en la app
     return false;
 });
 
 beforeEach(() => {
-    // Bloquear trackers usando patrones de URL que Cypress sí acepta
     cy.intercept('**/*backtrace.io/**', { statusCode: 200, body: {} });
     cy.intercept('**/*google-analytics.com/**', { statusCode: 200, body: {} });
 });
